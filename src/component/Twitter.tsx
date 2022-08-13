1b7c7ce558ac8bfd78c7dddd85dc8e3f7c1dc987
kyoichi1 <s206cg06k@yahoo.co.jp>
2022-08-13 23:34:02 +0900

    ✨ Twitterコンポーネント作成

---
+++ src/component/Twitter.tsx
@@ -0,0 +1,169 @@
+import Image from "next/image";
+import React from "react";
+
+const Twitter = () => {
+  return (
+    <div className="mx-4 mt-10 w-96 ">
+      <div className="my-4 text-3xl font-bold">
+        <h1>Twitter</h1>
+      </div>
+      <div className="border-t-2"></div>
+
+      {/*投稿内容 */}
+      <div className="">
+        <div className="mt-10  h-64 ">
+          <div className="items-center text-clip rounded font-['YuGothic']  font-light text-gray-700 ">
+            <div className="flex">
+              <div className="m-2">
+                <Image
+                  className="items-center rounded-full"
+                  src="/profile-shimabu.png"
+                  width={40}
+                  height={40}
+                  alt=""
+                />
+              </div>
+              <div className="w-80 text-ellipsis">
+                <div className="ml-2 flex items-center font-bold ">
+                  <p className=" overflow-hidden  font-bold ">
+                    しまぶーのIT大学
+                  </p>
+                  <p className=" ml-2 overflow-hidden  text-xs text-gray-500">
+                    @shimabu
+                  </p>
+                  <p className="ml-2 overflow-hidden  text-xs text-gray-500">
+                    5月25日
+                  </p>
+                </div>
+
+                <div className="mx-2 my-4 text-sm">
+                  <p>
+                    📣 新サービス「Noway Form」をリリースしました!
+                    <br />
+                    <br /> Noway
+                    Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogleFormsでやっていたことがNotionだけで完結します✌✨
+                    <br />
+                    <br />
+                    試しに使っていただけると幸いです😊
+                    <br />
+                    <br />
+                    <a
+                      className="text-blue-400"
+                      href="https://www.noway-form.com/ja"
+                    >
+                      https://www.noway-form.com/ja
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*投稿内容 */}
+      <div className="">
+        <div className="mt-10   h-64 ">
+          <div className="items-center text-clip rounded font-['YuGothic']  font-light text-gray-700 ">
+            <div className="flex">
+              <div className="m-2">
+                <Image
+                  className="items-center rounded-full"
+                  src="/profile-shimabu.png"
+                  width={40}
+                  height={40}
+                  alt=""
+                />
+              </div>
+              <div className="w-80 text-ellipsis">
+                <div className="ml-2 flex items-center font-bold ">
+                  <p className=" overflow-hidden  font-bold ">
+                    しまぶーのIT大学
+                  </p>
+                  <p className=" ml-2 overflow-hidden  text-xs text-gray-500">
+                    @shimabu
+                  </p>
+                  <p className="ml-2 overflow-hidden  text-xs text-gray-500">
+                    5月25日
+                  </p>
+                </div>
+
+                <div className="mx-2 my-4 text-sm">
+                  <p>
+                    📣 新サービス「Noway Form」をリリースしました!
+                    <br />
+                    <br /> Noway
+                    Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogleFormsでやっていたことがNotionだけで完結します✌✨
+                    <br />
+                    <br />
+                    試しに使っていただけると幸いです😊
+                    <br />
+                    <br />
+                    <a
+                      className="text-blue-400"
+                      href="https://www.noway-form.com/ja"
+                    >
+                      https://www.noway-form.com/ja
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*投稿内容 */}
+      <div className="">
+        <div className="mt-10  h-64 ">
+          <div className="items-center text-clip rounded font-['YuGothic']  font-light text-gray-700 ">
+            <div className="flex">
+              <div className="m-2">
+                <Image
+                  className="items-center rounded-full"
+                  src="/profile-shimabu.png"
+                  width={40}
+                  height={40}
+                  alt=""
+                />
+              </div>
+              <div className="w-80 text-ellipsis">
+                <div className="ml-2 flex items-center font-bold ">
+                  <p className=" overflow-hidden  font-bold ">
+                    しまぶーのIT大学
+                  </p>
+                  <p className=" ml-2 overflow-hidden  text-xs text-gray-500">
+                    @shimabu
+                  </p>
+                  <p className="ml-2 overflow-hidden  text-xs text-gray-500">
+                    5月25日
+                  </p>
+                </div>
+
+                <div className="mx-2 my-4 text-sm">
+                  <p>
+                    📣 新サービス「Noway Form」をリリースしました!
+                    <br />
+                    <br /> Noway
+                    Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogleFormsでやっていたことがNotionだけで完結します✌✨
+                    <br />
+                    <br />
+                    試しに使っていただけると幸いです😊
+                    <br />
+                    <br />
+                    <a
+                      className="text-blue-400"
+                      href="https://www.noway-form.com/ja"
+                    >
+                      https://www.noway-form.com/ja
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Twitter;

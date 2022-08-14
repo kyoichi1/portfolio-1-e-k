c06ea780e2a6e98660a239ca71f8adfda5c4b6a1
kyoichi1 <s206cg06k@yahoo.co.jp>
2022-08-15 00:08:27 +0900

    ✨ contactページ作成

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="mx-4 mt-10">
+    <div className="m-4 mt-10 sm:ml-56 sm:mr-32 min-w-min sm:w-auto">
       <div className="my-4 text-3xl font-bold text-gray-800">
         <h1>Contact</h1>
       </div>
       <div className="border-t-2"></div>
-      <div className="Class Preview mt-4  h-[119px]">
-        <div className="font-['YuGothic'] text-[22px]  font-bold text-gray-700"></div>
-        <div className="text-gray-80  h-[ 50px] font-[ 'YuGothic'] mt-2 text-clip text-gray-700">
-          <div className="w-[358px] items-center justify-center ">
+      <div className="Class Preview mt-4  h-28 ">
+        <div className="font-['YuGothic'] text-2xl  font-bold text-gray-700"></div>
+        <div className="text-gray-80  h-12 font-['YuGothic'] mt-2 text-clip text-gray-700 ">
+          <div className="w-[358px] items-center justify-center sm:w-auto ">
             <label className="block text-xs font-semibold">Email</label>
             <input
               className=" mt-1 h-8 w-full rounded-sm border-2 border-gray-200 pl-2 text-sm text-gray-300 "
@@ -21,10 +21,10 @@ const Contact = () => {
             />
           </div>
 
-          <div className="mt-10 w-[358px] items-center justify-center">
+          <div className="mt-10 w-[358px] items-center justify-center sm:w-auto ">
             <label className="block text-xs font-semibold">Name</label>
             <input
-              className=" mt-1 h-8 w-full rounded-sm border-2 border-gray-200 pl-2 text-sm text-gray-300 "
+              className="mt-1 h-8 w-full rounded-sm border-2 border-gray-200 pl-2 text-sm text-gray-300 "
               type="text"
               name=""
               id=""
@@ -32,10 +32,10 @@ const Contact = () => {
             />
           </div>
 
-          <div className="mt-10 w-[358px] items-center justify-center">
+          <div className="mt-10 w-[358px] items-center justify-center sm:w-auto ">
             <label className="block text-xs font-semibold">Name</label>
             <textarea
-              className=" mt-1 h-24 w-full rounded-sm border-2 border-gray-200  pl-2 text-sm text-gray-300"
+              className="mt-1 h-24 w-full rounded-sm border-2 border-gray-200  pl-2 text-sm text-gray-300"
               name=""
               id=""
               value={"I want to order your goods"}
@@ -43,8 +43,8 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <div className="mx-4 mt-64 flex w-96 items-center justify-center  ">
-        <button className="rounded-3xl bg-gray-800 py-[10px] px-[20px] text-base text-white">
+      <div className="mt-64 flex w-96 items-center justify-center  sm:items-center sm:w-auto">
+        <button className="rounded-3xl bg-gray-800 py-2 px-5 text-base text-white">
           Send message
         </button>
       </div>

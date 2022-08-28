e740fda37f3e517539715d7cc2e751b7c1ef5fee
kyoichi1 <s206cg06k@yahoo.co.jp>
2022-08-28 15:04:29 +0900

    🥅 localhost3000のエラー修正

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -5,7 +5,11 @@ export const client = createClient({
   serviceDomain: "kyoichi-it-blog",
    // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
   // apiKey: "process.env.NEXT_PUBLIC_API_KEY",
-  apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+
+  //これでvercleのデプロは成功
+  // apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+
+  apiKey: `${process.env.API_KEY}`,
   // apiKey: `${process.env.API_KEY}`,
   // apiKey: process.env.API_KEY,
 

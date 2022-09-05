213b6b08827b1e62595220e7640cf821b2e2103b
kyoichi <s206cg06k@yahoo.co.jp>
2022-09-05 10:38:03 +0900

    🔥  Blogをコンポーネントに切り出し（作業中）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
-import type {NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
 import BlogList from "src/component/Blog/BlogList";
 import Button from "src/component/Button";
 import GitHub from "src/component/GitHub";
 import Portfolio from "src/component/Portfolio";
 import Title from "src/component/Title";
 import Twitter from "src/component/Twitter";
+import { client } from "src/libs/client";
+import { Blog } from "./blog";
 
-const HOME: NextPage = () => {
+type Props = MicroCMSListResponse<Blog>;
 
+const HOME: NextPage<Props> = (props) => {
   return (
     <div className="m-auto w-96 min-w-min items-center sm:w-auto">
       <Title />
-      <BlogList />
+      {/*TODO  totalCount={0} limit={0} offset={0} BlogListの型を設定して左記は消したい*/}
+      <BlogList contents={props.contents} totalCount={0} limit={0} offset={0} />
       <Button title="View All" />
       <Portfolio />
       <Button title="View All" />
-
       <div className="justify-between sm:mx-52 sm:flex ">
         <div className="w-96 ">
           <GitHub />
@@ -44,3 +48,10 @@ const HOME: NextPage = () => {
 };
 
 export default HOME;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+  return {
+    props: data,
+  };
+};

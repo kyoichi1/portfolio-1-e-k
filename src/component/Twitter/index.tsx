6b94dcaae72358cfc3fee3cc2485b290a98231b1
kyoichi <s206cg06k@yahoo.co.jp>
2022-09-12 21:20:21 +0900

    ✨ TwitterAPIを実装しました

--- src/component/Twitter/index.tsx
+++ src/component/Twitter/index.tsx
@@ -1,106 +1,70 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+import dayjs from "dayjs";
 import React, { FC } from "react";
 
-/* データ受け取りの際にこの型が使えれば使う予定（モック用/現在はporpsで受取してないので利用していない） */
-// type TwitterProps = {
-//   userName: string;
-//   userAccount: string;
-//   userImage: string;
-//   text: string;
-//   image?: string;
-//   date: string;
-//   link?: string;
-// };
-
-/* TODO 今はデータをここに書いてMapを使っているが、APIからデータを取得に変更する */
-const TWITTER = [
-  {
-    userName: "しまぶーのIT大学",
-    userAccount: "shimabu",
-    userImage: "/Twitter-shimabu.png",
-    text: "",
-    date: "5月25日",
-    link: "https://www.noway-form.com/ja",
-  },
-  {
-    userName: "しまぶーのIT大学",
-    userAccount: "shimabu",
-    userImage: "/Twitter-shimabu.png",
-    text: "",
-    date: "5月26日",
-    link: "https://www.noway-form.com/ja",
-  },
-  {
-    userName: "しまぶーのIT大学",
-    userAccount: "shimabu",
-    userImage: "/Twitter-shimabu.png",
-    text: "",
-    date: "5月27日",
-    link: "https://www.noway-form.com/ja",
-  },
-];
-
-export const Twitter = () => {
+// TODO:SDKの中の型を見つけて型付けが必要（見つかる気配相当薄い...）
+type Props = {
+  twitter: any;
+};
+export const Twitter: FC<Props> = (props) => {
   return (
-    <div className="mx-4 mt-10 w-96 ">
+    <div className="mx-4 mt-10 w-96 sm:ml-9">
       <div className="my-4 text-3xl font-bold">
         <h1>Twitter</h1>
       </div>
       <div className="border-t-2"></div>
-
-      {TWITTER.map((twitter) => {
-        return (
-          <div key={twitter.date} className="">
-            <div className="mt-10  h-64 ">
-              <div className="items-center text-clip rounded font-['YuGothic']  font-light text-gray-700 ">
-                <div className="flex">
-                  <div className="m-2">
-                    <Image
-                      className="items-center rounded-full"
-                      src={twitter.userImage}
-                      width={40}
-                      height={40}
-                      alt=""
-                    />
-                  </div>
-                  <div className="w-80 text-ellipsis">
-                    <div className="ml-2 flex items-center font-bold ">
-                      <p className=" overflow-hidden  font-bold ">
-                        {twitter.userName}
-                      </p>
-                      <p className=" ml-2 overflow-hidden  text-xs text-gray-500">
-                        {`@${twitter.userAccount}`}
-                      </p>
-                      <p className="ml-2 overflow-hidden  text-xs text-gray-500">
-                        {twitter.date}
-                      </p>
+      <div className="h-[878px] overflow-y-auto">
+        {props.twitter.tweetlist.data.map((value: any) => {
+          return (
+            <div key={value.id}>
+              <div className="mt-10 ">
+                <div className="items-center text-clip rounded font-['YuGothic']  font-light text-gray-700 ">
+                  <div className="flex">
+                    <div className="m-2">
+                      {/* TODO:Next/Imageは基本このまま使うとエラーとなるが、ゴリゴリすると設定できるとのことなので変更対応できればしたい*/}
+                      {/* <Image
+                          className="items-center rounded-full bg-blue-600"
+                          src="{props.tw.user.data.profile_image_url}"
+                          width={40}
+                          height={40}
+                          alt=""
+                        /> */}
+                      <img
+                        className="h-[40px] w-[40px] items-center rounded-full"
+                        src={props.twitter.user.data.profile_image_url}
+                        alt=""
+                      ></img>
                     </div>
-
-                    <div className="mx-2 my-4 text-sm">
-                      <p>
-                        📣 新サービス「Noway Form」をリリースしました!
-                        <br />
-                        <br /> Noway
-                        Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogleFormsでやっていたことがNotionだけで完結します✌✨
-                        <br />
-                        <br />
-                        試しに使っていただけると幸いです😊
-                        <br />
-                        <br />
-                        {twitter.link ? (
-                          <a className="text-blue-400" href={twitter.link}>
-                            {twitter.link}
-                          </a>
-                        ) : null}
-                      </p>
+                    <div className="mt-4 w-80 text-ellipsis">
+                      <div className="ml-2 flex items-center font-bold ">
+                        <p className=" overflow-hidden  font-bold ">
+                          {props.twitter.user.data.name}
+                        </p>
+                        <p className=" ml-2 overflow-hidden  text-xs text-gray-500">
+                          {`@${props.twitter.user.data.username}`}
+                        </p>
+                        {/* TODO：取ってこれてる日程データが違う気がします...修正予定*/}
+                        <p className="ml-2 overflow-hidden  text-xs text-gray-500">
+                          {dayjs(props.twitter.user.data.created_at).format(
+                            "MM月DD日"
+                          )}
+                        </p>
+                      </div>
+                      <div className="mx-2 my-4 text-sm">
+                        {/*TODO： dangerouslySetInnerHTMLが効いていない...<a>タグと＃を表示できるように調整が必要 */}
+                        <div
+                          className=""
+                          dangerouslySetInnerHTML={{ __html: value.text }}
+                        ></div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/libs/twitter/twiiterClient.ts
+++ src/libs/twitter/twiiterClient.ts
@@ -1,3 +1,14 @@
 import { Client } from "twitter-api-sdk";
 
-const twiiterClient = new Client("MY-BEARER-TOKEN");
+export const twitterClient = new Client(process.env.TWITTER_BEARER_TOKEN ?? "");
+export async function getTweetList() {
+  const user = await twitterClient.users.findUserByUsername("kyoichi_1", {
+    "user.fields": ["profile_image_url"],
+  });
+
+  if (!user.data?.id) {
+    return;
+  }
+  const tweetlist = await twitterClient.tweets.usersIdTweets(user.data.id);
+  return { user, tweetlist };
+}
